c8bed72fb9ff960d349c3d24827a6641ac26736e
Boozy044 <boozy044@gmail.com>
2024-03-20 10:36:13 +0300

    chore: add envs for api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(Logger));
+
+  app.setGlobalPrefix('/api');
+
   await app.listen(3000);
 }
 bootstrap();
